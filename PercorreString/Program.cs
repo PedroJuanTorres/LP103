e5c85a47bdb28eb727b6fdc2288aa04853588359
Letícia Oliveira Torres <leticia@MacBook-Air-de-Leticia.local>
2021-03-10 09:58:12 +0000

    Solving PercorreString exercise

---
+++ PercorreString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            string inputString;
+            char inputChar;
+
+            //Ask user for a string
+            Console.Write("Write a string: ");
+            inputString = Console.ReadLine();
+
+            //Ask user for a character
+            Console.Write("Write a char: ");
+            inputChar = Console.ReadLine()[0];
+
+            // Write thr string without the character
+            foreach (char c in inputString)
+            {
+                if (c != inputChar)
+                {
+                    Console.Write(c);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
